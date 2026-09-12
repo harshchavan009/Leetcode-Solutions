7670437c12e078d53a76039cebb39054536e67e5
Harsh Chavan <harshchavan1030@gmail.com>
2026-09-12 22:34:03 +0530

    Time: 4677 ms (93.6%), Space: 12.5 MB (24.17%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,39 @@
+class Solution:
+    def exist(self, board, word):
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r, c, index):
+            if index == len(word):
+                return True
+
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return False
+
+            if board[r][c] != word[index]:
+                return False
+
+            # Mark the cell as visited
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            # Explore all 4 directions
+            found = (
+                dfs(r + 1, c, index + 1) or
+                dfs(r - 1, c, index + 1) or
+                dfs(r, c + 1, index + 1) or
+                dfs(r, c - 1, index + 1)
+            )
+
+            # Restore the cell
+            board[r][c] = temp
+
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0]:
+                    if dfs(r, c, 0):
+                        return True
+
+        return False
