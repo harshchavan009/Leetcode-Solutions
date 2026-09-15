a7c6ff07d2c49a39715c123deede39c3dcd52e95
Harsh Chavan <harshchavan1030@gmail.com>
2026-09-15 20:51:20 +0530

    Time: 22 ms (96.9%), Space: 14.7 MB (74.82%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def connect(self, root):
+        if root is None:
+            return None
+
+        current = root
+
+        while current:
+            # Dummy node helps build the next level
+            dummy = Node(0)
+            tail = dummy
+
+            # Traverse current level using already-built next pointers
+            while current:
+                if current.left:
+                    tail.next = current.left
+                    tail = tail.next
+
+                if current.right:
+                    tail.next = current.right
+                    tail = tail.next
+
+                current = current.next
+
+            # Move to the first node of the next level
+            current = dummy.next
+
+        return root
