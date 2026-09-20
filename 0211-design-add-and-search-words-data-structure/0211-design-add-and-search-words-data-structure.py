847adde0ea6b464fa7980be92214af416de6eec9
Harsh Chavan <harshchavan1030@gmail.com>
2026-09-20 09:49:44 +0530

    Time: 2623 ms (75.57%), Space: 117.5 MB (25.8%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+
+        node.end = True
+
+    def search(self, word):
+
+        def dfs(node, i):
+            if i == len(word):
+                return node.end
+
+            ch = word[i]
+
+            if ch == '.':
+                for child in node.children.values():
+                    if dfs(child, i + 1):
+                        return True
+                return False
+
+            if ch not in node.children:
+                return False
+
+            return dfs(node.children[ch], i + 1)
+
+        return dfs(self.root, 0)
