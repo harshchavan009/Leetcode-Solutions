e10ac4b0ec779fe54aebe2bffea872f4958c4fd3
Harsh Chavan <harshchavan1030@gmail.com>
2026-09-24 10:40:38 +0530

    Time: 157 ms (78.02%), Space: 25.8 MB (96.08%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximalSquare(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        dp = [0] * (n + 1)
+        max_side = 0
+
+        for i in range(1, m + 1):
+            prev = 0
+
+            for j in range(1, n + 1):
+                temp = dp[j]
+
+                if matrix[i - 1][j - 1] == '1':
+                    dp[j] = min(dp[j], dp[j - 1], prev) + 1
+                    max_side = max(max_side, dp[j])
+                else:
+                    dp[j] = 0
+
+                prev = temp
+
+        return max_side * max_side
